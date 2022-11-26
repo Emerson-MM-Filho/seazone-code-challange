bbc70e83335570b780616c2eac0652b6afe19272
Emerson Filho <emerson.dev.machado@gmail.com>
2022-11-26 16:37:50 -0300

    create BookingViewSet

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Booking
+from .serializers import BookingSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    """
+        API endpoint for booking CRUD.
+    """
+    queryset = Booking.objects.all().order_by('-created_at')
+    serializer_class = BookingSerializer
