d2916c5252971bdfb7c51ac92ee852ea2e2c7d4b
Emerson Filho <emerson.dev.machado@gmail.com>
2022-11-26 16:30:36 -0300

    add router and propriety route

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
 
+from rest_framework import routers
 from rest_framework.schemas import get_schema_view
+
+from propriety import views as propriety_views
+
+router = routers.DefaultRouter()
+router.register(r'propriety', propriety_views.ProprietyViewSet, basename='propriety')
+
 urlpatterns = [
+    path('', include(router.urls)),
     path('schema', get_schema_view(title='API Schema', description='Guide for the Rest API'), name='schema'),
     path('docs/', TemplateView.as_view(
         template_name='docs.html',
