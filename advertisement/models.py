a7c7c9d43504a72dfeac0af58784871b5b6ea7c2
Emerson Filho <emerson.dev.machado@gmail.com>
2022-11-26 14:15:44 -0300

    create Advertisement model

--- advertisement/models.py
+++ advertisement/models.py
@@ -2,4 +2,10 @@ from re import M
 from django.db import models
 from propriety.models import Propriety
 
-# Create your models here.
+
+class Advertisement(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, blank=False, null=False)
+    updated_at = models.DateTimeField(auto_now=True, blank=False, null=False)
+    property_id = models.ForeignKey(Propriety, on_delete=models.CASCADE)
+    platform = models.CharField(max_length=250, null=False, blank=False)
+    platform_fee = models.DecimalField(decimal_places=2, max_digits=9, null=False, blank=False)
