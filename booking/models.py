e6b2b34620e471f0dcf1cf649b4cd072cae5c8c3
Emerson Filho <emerson.dev.machado@gmail.com>
2022-11-26 14:17:12 -0300

    create Booking model

--- booking/models.py
+++ booking/models.py
@@ -6,3 +6,14 @@ from django.db import models
 def generate_random_code(cls):
     chars = "abcdefghijklmnopqrstuvwxyz0123456789"
     return "".join(chars[c % len(chars)] for c in urandom(6))
+
+class Booking(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, blank=False, null=False)
+    updated_at = models.DateTimeField(auto_now=True, blank=False, null=False)
+    code = models.UUIDField(blank=False, null=False, editable=False, default=generate_random_code)
+    advertisement_id = models.ForeignKey(Advertisement, on_delete=models.CASCADE, null=False, blank=False)
+    check_in = models.DateField(null=False, blank=False)
+    check_out = models.DateField(null=False, blank=False)
+    price = models.DecimalField(decimal_places=2, max_digits=9, null=False, blank=False)
+    commentary = models.CharField(max_length=250, null=False, blank=False)
+    guests_count = models.IntegerField(null=False, blank=False)
