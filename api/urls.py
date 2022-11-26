dd068e04d65a53a2079e3c03299c986e1696eb9e
Emerson Filho <emerson.dev.machado@gmail.com>
2022-11-26 16:28:47 -0300

    add docs url

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path, include
+from django.views.generic import TemplateView
+
 from rest_framework.schemas import get_schema_view
 urlpatterns = [
     path('schema', get_schema_view(title='API Schema', description='Guide for the Rest API'), name='schema'),
+    path('docs/', TemplateView.as_view(
+        template_name='docs.html',
+        extra_context={'schema_url':'schema'}
+    ), name='docs')
 ]
