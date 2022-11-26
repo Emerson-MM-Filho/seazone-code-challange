9d45cd6908f9dfcafcce2a18a43a5bcc7b7381f3
Emerson Filho <emerson.dev.machado@gmail.com>
2022-11-26 14:16:19 -0300

    init and install booking app

---
+++ booking/models.py
@@ -0,0 +1,5 @@
+from os import urandom
+from advertisement.models import Advertisement
+from django.db import models
+
+# Create your models here.
--- challenge/settings.py
+++ challenge/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'advertisement',
     'propriety',
+    'booking'
 ]
 
 MIDDLEWARE = [
