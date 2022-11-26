28574ce1717c66c8fdc586224bbb9f4a9f33b67a
Emerson Filho <emerson.dev.machado@gmail.com>
2022-11-26 14:16:59 -0300

    create generate_random_code funtion

--- booking/models.py
+++ booking/models.py
@@ -2,4 +2,7 @@ from os import urandom
 from advertisement.models import Advertisement
 from django.db import models
 
-# Create your models here.
+
+def generate_random_code(cls):
+    chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+    return "".join(chars[c % len(chars)] for c in urandom(6))
