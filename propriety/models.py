76777f1bc0819dbf227dc988ec246658a9a53935
Emerson Filho <emerson.dev.machado@gmail.com>
2022-11-26 14:14:13 -0300

    create Propriety model

--- propriety/models.py
+++ propriety/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Propriety(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, blank=False, null=False, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, blank=False, null=False, editable=False)
+    activation_date = models.DateTimeField(blank=False, null=False)
+    code = models.CharField(max_length=250, null=False, blank=False)
+    guests_limit = models.IntegerField(null=False, blank=False)
+    bathroom_count = models.IntegerField(null=False, blank=False)
+    pet_frendly = models.BooleanField(null=False, blank=False)
+    cleaner_fee = models.DecimalField(decimal_places=2, max_digits=9, null=False, blank=False)
