f95e9fa26256edd90e05a52590ba6fa14e442632
Emerson Filho <emerson.dev.machado@gmail.com>
2022-11-26 17:23:20 -0300

    add checkin and checkout validation

--- booking/serializers.py
+++ booking/serializers.py
@@ -1,3 +1,4 @@
+from datetime import date
 from rest_framework import serializers
 from .models import Booking
 
@@ -6,3 +7,14 @@ class BookingSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Booking
         fields = "__all__"
+
+    def is_valid(self, *args, **kwargs):
+        data = self._kwargs['data']
+
+        check_in_date = date.fromisoformat(data['check_in'])
+        check_out_date = date.fromisoformat(data['check_out'])
+
+        if check_in_date > check_out_date:
+            raise serializers.ValidationError('The check-out date must be later than the check-in date')
+
+        super().is_valid(*args, **kwargs)
