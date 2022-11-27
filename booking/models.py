993832243b97b3f25b9b5e6ae456e3fdad55fc23
Emerson Filho <emerson.dev.machado@gmail.com>
2022-11-27 12:31:12 -0300

    change field name

--- booking/models.py
+++ booking/models.py
@@ -10,7 +10,7 @@ class Booking(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, blank=False, null=False)
     updated_at = models.DateTimeField(auto_now=True, blank=False, null=False)
     code = models.CharField(max_length=BOOKING_CODE_MAX_LENGTH, blank=False, null=False, editable=False, default=generate_booking_code, unique=True)
-    advertisement_id = models.ForeignKey(Advertisement, on_delete=models.CASCADE, null=False, blank=False)
+    advertisement = models.ForeignKey(Advertisement, on_delete=models.CASCADE, null=False, blank=False)
     check_in = models.DateField(null=False, blank=False)
     check_out = models.DateField(null=False, blank=False)
     price = models.DecimalField(decimal_places=2, max_digits=9, null=False, blank=False)
