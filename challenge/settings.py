7bb3c70ecc6b55c028d1581dc5f785103bf80b79
Emerson Filho <emerson.dev.machado@gmail.com>
2022-11-27 19:39:19 -0300

    set TEST_REQUEST_DEFAULT_FORMAT as json

--- challenge/settings.py
+++ challenge/settings.py
@@ -137,6 +137,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
     'COERCE_DECIMAL_TO_STRING': False,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
