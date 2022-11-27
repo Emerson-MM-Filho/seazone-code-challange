43ebdc814929008b20ef9bb3e13b3eef70f360bd
Emerson Filho <emerson.dev.machado@gmail.com>
2022-11-27 12:29:24 -0300

    change field name and __str__ method

--- advertisement/models.py
+++ advertisement/models.py
@@ -1,4 +1,3 @@
-from re import M
 from django.db import models
 from propriety.models import Propriety
 
@@ -6,9 +5,9 @@ from propriety.models import Propriety
 class Advertisement(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, blank=False, null=False)
     updated_at = models.DateTimeField(auto_now=True, blank=False, null=False)
-    property_id = models.ForeignKey(Propriety, on_delete=models.CASCADE)
+    propriety = models.ForeignKey(Propriety, on_delete=models.CASCADE)
     platform = models.CharField(max_length=250, null=False, blank=False)
     platform_fee = models.DecimalField(decimal_places=2, max_digits=9, null=False, blank=False)
 
     def __str__(self) -> str:
-        return str(self.pk)
+        return f"{self.pk} - {self.platform} - {self.propriety.code}"
