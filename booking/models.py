1f3f5891718b49d93e556e6ac715fe5f779e52f2
Emerson Filho <emerson.dev.machado@gmail.com>
2022-11-27 19:41:07 -0300

    create BookingManager and get_unavailable_dates_by_propriety method

--- booking/models.py
+++ booking/models.py
@@ -1,12 +1,29 @@
+from datetime import date
+from typing import Set
+import advertisement
 from advertisement.models import Advertisement
 from django.db import models
-from challenge.utils import generate_random_code
+from challenge.utils import get_date_range_list, generate_random_code
+from propriety.models import Propriety
 
 BOOKING_CODE_MAX_LENGTH = 6
 def generate_booking_code():
     return generate_random_code(BOOKING_CODE_MAX_LENGTH)
 
+class BookingManager(models.Manager):
+
+    def get_unavailable_dates_by_propriety(self, propriety: Propriety) -> Set[date]:
+        bookings = self.model.objects.filter(
+            advertisement__propriety=propriety
+        )
+        unavailable_dates = set()
+        for booking in bookings:
+            unavailable_dates.update(get_date_range_list(booking.check_in, booking.check_out))
+        return unavailable_dates
+
 class Booking(models.Model):
+    objects = BookingManager()
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     code = models.CharField(max_length=BOOKING_CODE_MAX_LENGTH, editable=False, default=generate_booking_code, unique=True)
