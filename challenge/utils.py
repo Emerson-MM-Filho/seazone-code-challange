47e6d3fe2fe4b1fdeae42e928f1794f01faef902
Emerson Filho <emerson.dev.machado@gmail.com>
2022-11-27 19:39:30 -0300

    create get_date_range_list

--- challenge/utils.py
+++ challenge/utils.py
@@ -1,5 +1,17 @@
 from os import urandom
+from datetime import date, timedelta
+from typing import Set
 
 def generate_random_code(length: int) -> str:
+    """Generate a random code with the given length"""
     chars = "abcdefghijklmnopqrstuvwxyz0123456789"
     return "".join(chars[c % len(chars)] for c in urandom(length))
+
+def get_date_range_list(start_date: date, end_date: date) -> Set[date]:
+    """Return list of datetime.date objects between start_date and end_date (inclusive)"""
+    date_set = set()
+    curr_date = start_date
+    while curr_date <= end_date:
+        date_set.add(curr_date)
+        curr_date += timedelta(days=1)
+    return date_set
