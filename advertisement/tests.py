58ea731bae2b71147191d8646574b8c6b4d17b41
Emerson Filho <emerson.dev.machado@gmail.com>
2022-11-28 20:16:50 -0300

    create TestAdvertisement

--- advertisement/tests.py
+++ advertisement/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from advertisement.models import Advertisement
+from advertisement.serializers import AdvertisementSerializer
 
-# Create your tests here.
+
+class TestAdvertisement(APITestCase):
+    url = "/advertisement/"
+
+    fixtures = ["initial_data.json"]
+
+    def test_get_advertisement_api(self):
+        response = self.client.get(self.url)
+        expected_result = [AdvertisementSerializer(advertisement).data for advertisement in Advertisement.objects.active()]
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(response.data, dict)
+        self.assertEqual(response.data["results"], expected_result)
+
+    def test_delete_advertisement_api(self):
+        advertisement = Advertisement.objects.first()
+        response = self.client.delete(f"{self.url}{advertisement.pk}/")
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_advertisement_instance(self):
+        advertisement = Advertisement.objects.first()
+        advertisement.delete()
+
+        self.assertEqual(advertisement.active, False)
