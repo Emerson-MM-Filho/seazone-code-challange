9d51bc00edd918aeef92b9e8a1ed310c8e46a62c
Emerson Filho <emerson.dev.machado@gmail.com>
2022-11-28 18:54:26 -0300

    adjust Advertisement model to use ActiveManager and add active field

--- advertisement/models.py
+++ advertisement/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from propriety.models import Propriety
-
+from api.managers import ActiveManager
 
 class Advertisement(models.Model):
+    objects = ActiveManager()
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     propriety = models.ForeignKey(Propriety, on_delete=models.CASCADE)
     platform = models.CharField(max_length=250)
     platform_fee = models.DecimalField(decimal_places=2, max_digits=9)
+    active = models.BooleanField(default=True)
 
     def __str__(self) -> str:
         return f"{self.pk} - {self.platform} - {self.propriety.code}"
